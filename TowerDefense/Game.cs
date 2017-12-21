b9857dc6b51c2dd3095e46e02c5945720a9469c0
James Lockhart <james.lockhart117@gmail.com>
2017-12-21 14:36:33 +0300

    Added basic classes to define game objects

---
+++ TowerDefense/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerDefense
+{
+    class Game
+    {
+        public void Main()
+        {
+            Tower tower = new Tower();
+        }
+    }
+}
